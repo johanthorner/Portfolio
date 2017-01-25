96cbb723d7422857192e8d6eed20aff960c59e87
johan thöner <johan@thorner.se>
2017-01-25 17:12:27 +0100

    sista commiten cloudnine

---
+++ Presentation/PresentationEpiserver/Models/Blocks/SpacerBlock.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+
+namespace PresentationEpiserver.Models.Blocks
+{
+    [ContentType(DisplayName = "SpacerBlock", GUID = "513ed1ea-64ec-4068-9d6b-56928dac96da", Description = "")]
+    public class SpacerBlock : BlockData
+    {
+        /*
+                [CultureSpecific]
+                [Display(
+                    Name = "Name",
+                    Description = "Name field's description",
+                    GroupName = SystemTabNames.Content,
+                    Order = 1)]
+                public virtual string Name { get; set; }
+         */
+    }
+}
